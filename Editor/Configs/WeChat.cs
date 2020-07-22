463a98a905740ed93dd92773ab68c13c0829de8f
张路欣 <117408802@qq.com>
2020-07-22 14:35:37 +0800

    :art:修改代码结构，重新设计config文件

---
+++ Editor/Configs/WeChat.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace UnityThirdPartySdkManager.Editor.Configs
+{
+    /// <summary>
+    /// 微信结构体
+    /// </summary>
+    [Serializable]
+    public class WeChat
+    {
+        /// <summary>
+        /// appid
+        /// </summary>
+        public string appId;
+
+        /// <summary>
+        /// ulink
+        /// </summary>
+        public string ulink;
+    }
+}
