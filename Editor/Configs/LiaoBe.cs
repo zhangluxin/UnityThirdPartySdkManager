5313c05eaac81b811b818979d82fcbd1930a5897
张路欣 <117408802@qq.com>
2020-07-22 19:26:28 +0800

    :sparkles:添加各种sdk结构体和ui配置

---
+++ Editor/Configs/LiaoBe.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace UnityThirdPartySdkManager.Editor.Configs
+{
+    /// <summary>
+    /// 聊呗
+    /// </summary>
+    [Serializable]
+    public class LiaoBe
+    {
+        /// <summary>
+        /// app id
+        /// </summary>
+        public string appId;
+    }
+}
